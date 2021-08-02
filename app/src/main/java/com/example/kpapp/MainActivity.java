5003af2edf288d8f7f5f573a0763c076908e0c66
boycakra <boycakra69@gmail.com>
2021-08-02 20:53:58 +0700

    first commit

--- app/src/main/java/com/example/kpapp/MainActivity.java
+++ app/src/main/java/com/example/kpapp/MainActivity.java
@@ -2,26 +2,25 @@ package com.example.kpapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
 import android.content.Intent;
-
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
+        ImageButton imageView1 = (ImageButton) findViewById(R.id.imageView1);
+        imageView1.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void run() {
-                startActivity(new Intent(MainActivity.this,Menu.class));
-                finish();
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Menu.class);
+                startActivity(intent);
             }
-        },1500);
+        });
     }
 }
--- app/src/main/java/com/example/kpapp/Menu.java
+++ app/src/main/java/com/example/kpapp/Menu.java
@@ -1,73 +1,15 @@
 package com.example.kpapp;
 
-import android.content.Intent;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageView;
-import android.net.Uri;
-
-import com.smarteist.autoimageslider.IndicatorView.animation.type.IndicatorAnimationType;
-import com.smarteist.autoimageslider.SliderAnimations;
-import com.smarteist.autoimageslider.SliderView;
-
-import androidx.appcompat.app.AppCompatActivity;
+import android.widget.Button;
+import android.content.Intent;
+import android.widget.ImageButton;
 public class Menu extends AppCompatActivity {
-    SliderView sliderView;
-    int[] images = {R.drawable.kura,
-            R.drawable.dive1,
-            R.drawable.ikan,
-            R.drawable.karang,
-            R.drawable.ikan1,
-            R.drawable.karang1};
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu);
-        sliderView = findViewById(R.id.image_slider);
-
-        SliderAdapter sliderAdapter = new SliderAdapter(images);
-
-        sliderView.setSliderAdapter(sliderAdapter);
-        sliderView.setIndicatorAnimation(IndicatorAnimationType.WORM);
-        sliderView.setSliderTransformAnimation(SliderAnimations.DEPTHTRANSFORMATION);
-        sliderView.startAutoCycle();
-
-        ImageView joinnow = (ImageView)findViewById(R.id.joinbutton);
-        joinnow.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setAction(Intent.ACTION_VIEW);
-                intent.addCategory(Intent.CATEGORY_BROWSABLE);
-                intent.setData(Uri.parse("https://indonesia-biru.com/join-us/"));
-                startActivity(intent);
-            }
-        });
-
-        ImageView imageView1 = (ImageView) findViewById(R.id.text2);
-        imageView1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Menu.this, DivesiteActivity.class);
-                startActivity(intent);
-            }
-        });
-        ImageView imageview2 = (ImageView) findViewById(R.id.logo);
-        imageview2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Menu.this, About.class);
-                startActivity(intent);
-            }
-        });
-        ImageView imageView3 = (ImageView) findViewById(R.id.text3);
-        imageView3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Menu.this,MapsActivity.class);
-                startActivity(intent);
-            }
-        });
     }
 }
